280b5bf7c5c0090fdbb9ece731db753e7df0dd15
Tyler Becks <tylerdbecks@gmail.com>
2020-11-07 09:52:13 -0600

    Add house to cache from form page (#23)

--- pages/form.tsx
+++ pages/form.tsx
@@ -3,16 +3,16 @@ import { useSession } from 'next-auth/client';
 import { useRouter } from 'next/router';
 
 import NewHouseForm from '../components/new-house-form';
-import { UNSAVED_HOUSE_FRAGMENT } from '../fragments/house';
+import { HOUSE_FRAGMENT } from '../fragments/house';
 import { UnsavedHouse } from '../types/house';
 
 const ADD_HOUSE = gql`
   mutation AddHouse($house: houses_insert_input!) {
     insert_houses_one(object: $house) {
-      ...UnsavedHouse
+      ...House
     }
   }
-  ${UNSAVED_HOUSE_FRAGMENT}
+  ${HOUSE_FRAGMENT}
 `;
 
 const FormPage: React.FC = () => {
@@ -20,10 +20,28 @@ const FormPage: React.FC = () => {
   const [session] = useSession();
   const [addHouse] = useMutation(ADD_HOUSE);
 
-  const onSubmit = (house: UnsavedHouse) => {
+  const onSubmit = (unsavedNewHouse: UnsavedHouse) => {
     addHouse({
-      variables: {
-        house: { ...house, userId: session.user.id },
+      variables: { house: { ...unsavedNewHouse, userId: session.user.id } },
+      update: (cache, { data: { insert_houses_one: newHouse } }) => {
+        cache.modify({
+          fields: {
+            houses(existingHouseRefs = []) {
+              const newHouseRef = cache.writeFragment({
+                data: newHouse,
+                fragment: gql`
+                  fragment NewHouse on House {
+                    ...House
+                  }
+                  ${HOUSE_FRAGMENT}
+                `,
+                fragmentName: 'NewHouse',
+              });
+
+              return [...existingHouseRefs, newHouseRef];
+            },
+          },
+        });
       },
     });
 
