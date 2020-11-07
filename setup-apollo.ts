30f5efeefdef725e46facc68930136ad5ac47a42
Tyler Becks <tylerdbecks@gmail.com>
2020-11-07 07:46:50 -0800

    Add house to cache from form page

--- setup-apollo.ts
+++ setup-apollo.ts
@@ -1,10 +1,12 @@
 import { ApolloClient, InMemoryCache, NormalizedCacheObject } from '@apollo/client';
 
+const cache = new InMemoryCache();
+
 export const createApolloClient = (
   headers: Record<string, string>
 ): ApolloClient<NormalizedCacheObject> =>
   new ApolloClient({
     uri: 'https://moral-akita-32.hasura.app/v1/graphql',
-    cache: new InMemoryCache(),
+    cache,
     headers,
   });
